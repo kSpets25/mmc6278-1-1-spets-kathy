09eab682df2fbba8b43a30132897f2eda318cd91
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-05-24 17:46:10 -0500

    added js code to get the quote and add a quote

--- index.js
+++ index.js
@@ -13,19 +13,48 @@ program
   .description("Retrieves a random quote")
   .action(async () => {
     // TODO: Pull a random quote from the quotes.txt file
-    // console log the quote and author
-    // You may style the text with chalk as you wish
+    const data = (await fs.readFile(QUOTE_FILE, 'utf-8'))
+    
+    const lines = data.split("\n")
+    const filteredLines = lines.filter(line => line)
+    const randomLine = filteredLines[Math.floor(Math.random()*filteredLines.length)]
+    const splitLine = randomLine.split("|")
+    
+      // console log the quote and author
+    const [quote,author] = splitLine
+    console.log(quote, author)
+      
+      // You may style the text with chalk as you wish
   });
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
+
     // TODO: Add the quote and author to the quotes.txt file
-    // If no author is provided,
-    // save the author as "Anonymous".
-    // After the quote/author is saved,
+    try {  
+      quote =  "Do one thing every day that scares you.|"
+      author = "Elenor Roosevelt\n"
+      if (!author) {
+        author = "Anonymous\n"}
+      await fs.appendFile(QUOTE_FILE, [quote,author])
+      console.log('Quote successfully added')
+      let newLines = [quote,author]
+      console.log(newLines)
+      //alert("Quote successfully added!")
+    } catch(err) {
+      
+      console.log(err)
+    }
+      console.log(quote,author)
+      console.log("Sucessfully added quote and author")
+    
+    // If no author is provided,  
+    // save the author as "Anonymous"
+    // After the quote/author is saved, 
     // alert the user that the quote was added.
+    //console.log('Successfully added quote and author')
     // You may style the text with chalk as you wish
     // HINT: You can store both author and quote on the same line using
     // a separator like pipe | and then using .split() when retrieving
